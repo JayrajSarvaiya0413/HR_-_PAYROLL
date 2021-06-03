e4a6a8146f36cd30ccb68910e8aa50d0994b7e05
JayrajSarvaiya0413 <jayrajsarvaiya0413@gmail.com>
2021-06-03 18:10:12 +0530

    Update empregister.php

--- empregister.php
+++ empregister.php
@@ -1,8 +1,8 @@
  <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "honestysayar";
+$host = 'b7e9vpcz6pwigs7koahx-mysql.services.clever-cloud.com:3306';  
+        $user = 'ug1ghfzsirn2crxm';  
+        $password = 'FKyYRnoEnBkdCK3SdTLr';  
+        $db_name = 'b7e9vpcz6pwigs7koahx';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
