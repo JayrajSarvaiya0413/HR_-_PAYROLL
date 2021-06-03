770f0685f4f9b7dfe068440a52d87a6cecb9129e
JayrajSarvaiya0413 <jayrajsarvaiya0413@gmail.com>
2021-06-03 08:26:18 +0530

    Update coonfing.php

--- coonfing.php
+++ coonfing.php
@@ -1,8 +1,8 @@
     <?php      
-        $host = "localhost";  
-        $user = "ohMWAJY6IL";  
-        $password = "ILi1AHLaLG";  
-        $db_name = "ohMWAJY6IL";  
+        $host = 'remotemysql.com:3306';  
+        $user = 'ohMWAJY6IL';  
+        $password = 'ILi1AHLaLG';  
+        $db_name = 'ohMWAJY6IL'; 
           
         $con = mysqli_connect($host, $user, $password, $db_name);  
         if(mysqli_connect_errno()) {  
