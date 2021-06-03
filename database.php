8b4681f7f195e905f7f126b34cdcd3d7a180cdde
JayrajSarvaiya0413 <jayrajsarvaiya0413@gmail.com>
2021-06-03 18:30:54 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,8 @@
 <?php
-$host = 'b7e9vpcz6pwigs7koahx-mysql.services.clever-cloud.com:3306';  
-        $user = 'ug1ghfzsirn2crxm';  
-        $password = 'FKyYRnoEnBkdCK3SdTLr';  
-        $db_name = 'b7e9vpcz6pwigs7koahx'; 
-          
-        $con = mysqli_connect($host, $user, $password, $db_name);  
+$url='b7e9vpcz6pwigs7koahx-mysql.services.clever-cloud.com:3306';
+$username='ug1ghfzsirn2crxm';
+$password='FKyYRnoEnBkdCK3SdTLr';
+$conn=mysqli_connect($url,$username,$password,"b7e9vpcz6pwigs7koahx");
 if(!$conn){
  die('Could not Connect My Sql:' .mysql_error());
 }
