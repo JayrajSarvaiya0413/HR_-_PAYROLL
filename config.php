c4f08a4e17ae893bb47bb947d2723bae873a0d10
JayrajSarvaiya0413 <jayrajsarvaiya0413@gmail.com>
2021-06-04 07:50:27 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'honestysayar');
+define('DB_SERVER','b7e9vpcz6pwigs7koahx-mysql.services.clever-cloud.com:3306');
+define('DB_USER','ug1ghfzsirn2crxm');
+define('DB_PASS' ,'FKyYRnoEnBkdCK3SdTLr');
+define('DB_NAME', 'b7e9vpcz6pwigs7koahx');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
